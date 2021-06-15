ec2c580c066afd5c30c9aa65068cb7e4fc0236a2
Jordan <mjordan0130@gmail.com>
2021-06-14 22:23:04 -0500

    started site

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+
 import Header1 from '../components/Header1.jsx'
 import Bio from '../components/Bio.jsx'
 import Links from'../components/Links.jsx'
